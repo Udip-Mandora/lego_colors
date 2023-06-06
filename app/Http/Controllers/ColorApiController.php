b98c9693e99eeff852d6bfd788c2402c467e5110
Adam <thomasadam83@hotmail.com>
2023-06-06 07:27:06 -0400

    Added dump of API information

--- app/Http/Controllers/ColorApiController.php
+++ app/Http/Controllers/ColorApiController.php
@@ -19,7 +19,7 @@ class ColorApiController extends Controller
         $response = Http::withOptions(['verify' => false])->get('https://rebrickable.com/api/v3/lego/colors/?key=' . $apiKey);
         $data = json_decode($response);
         $data = $data->results;
-        echo '<pre>';
+        // echo '<pre>';
         // print_r($data);
         // dd($data);
         // return view('/colors/index', ['data' => $data->results]);
@@ -29,9 +29,58 @@ class ColorApiController extends Controller
         $is_trans = [];
         $brickId = [];
         $index = 2;
+
         foreach ($data as $item) {
+
+            echo 'ID: '.$item->id.'<br>';
+            echo 'Name: '.$item->name.'<br>';
+            echo 'RGB: #'.$item->rgb.'<br>';
+
+            echo '<div style="width: 100px; height: 100px; background-color:#'.$item->rgb.';"></div>';
+
+            if(property_exists($item->external_ids, 'BrickOwl'))
+            {
+
+                $brick_owl = $item->external_ids->BrickOwl;
+
+                if(count($brick_owl->ext_ids))
+                {
+                    foreach($brick_owl->ext_ids as $key => $value)
+                    {
+                        echo 'BrickOwl Name: '.$brick_owl->ext_ids[$key].' - '.$brick_owl->ext_descrs[$key][0].'<br>';
+                    }
+                }
+
+            }
+            else
+            {
+                echo 'NO BRICKOWL ALTERNATIVE NAME<br>';
+            }
+
+            if(property_exists($item->external_ids, 'LEGO'))
+            {
+
+                $lego = $item->external_ids->LEGO;
+
+                if(count($lego->ext_ids))
+                {
+                    foreach($lego->ext_ids as $key => $value)
+                    {
+                        echo 'LEGO Name: '.$lego->ext_ids[$key].' - '.$lego->ext_descrs[$key][0].'<br>';
+                    }
+                }
+        
+            }
+            else
+            {
+                echo 'NO LEGO ALTERNATIVE NAME<br>';
+            }
+
+            echo '<hr>';
+
             // $extIds = $item->external_ids->LEGO->ext_ids;
             // $extIds = $item['external_ids']['BrickLink']['ext_ids'];
+            /*
             $item = (array)$item;
             if ($item['id'] >= 0) {
                 $ids[] = $item['id'];
@@ -44,7 +93,8 @@ class ColorApiController extends Controller
                 // $brickId[] = is_array($item) ? $item['external_ids']['BrickLink']['ext_ids'] : $item->external_ids->BrickLink->ext_ids;
                 // $extIds = $item['external_ids']->BrickLink->ext_ids;
             }
-            $id = $item['id'];
+            */
+            // $id = $item['id'];
             // $brickId[] = $item['$id' + 1]->external_ids;
         }
         // foreach ($data as $item) {
@@ -60,6 +110,7 @@ class ColorApiController extends Controller
         // print_r($ids);
         // print_r($extIds);
         // $name = $data[]->exter;
+        /*
         $externalIds = $data[$id]->external_ids->BrickLink->ext_ids;
         $extIds = $data[1]->external_ids->LEGO->ext_ids;
         $legoDes = $data[1]->external_ids->LEGO->ext_descrs;
@@ -83,6 +134,7 @@ class ColorApiController extends Controller
             // ]);
         }
         dd("data stored");
+        */
     }
 
 
